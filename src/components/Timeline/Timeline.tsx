0be949962f40e82f41b4c52de1cf6195889c5e32
Tottzi <tottzi@gmail.com>
2021-09-03 10:37:08 +0200

    the prev works updated

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -11,11 +11,13 @@ import "./Timeline.scss";
 interface projectInterface {
   Name: string;
   Desc: string;
-  weblink: string;
+  weblink?: string;
+  picture?: string;
+  github: string;
   techs: string[];
   className: string;
   contentStyle?: any;
-  contentArrowStyle: any;
+  contentArrowStyle?: any;
   iconStyle: any;
   icon: any;
 }
@@ -42,19 +44,29 @@ const Timeline = () => {
               iconStyle={project.iconStyle}
               icon={testIcon}
             >
-                <h3 className="vertical-timeline-element-title">
-                  {project.Name}
-                </h3>
-                {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-                <div className="thumbnail-container">
-                  <div className="thumbnail">
-                    <iframe src={project.weblink} frameBorder="0"></iframe>
-                  </div>
-                </div>
-                <p>{project.Desc}</p>
+              <h1 className="vertical-timeline-element-title">
+                {project.Name}
+              </h1>
+              {project.weblink ? (
+                <a target="_blank" rel="noreferrer" href={project.weblink}>
+                  <img
+                    className="vertical-timeline-element-image"
+                    alt={project.Name}
+                    src={project.picture}
+                  />
+                </a>
+              ) : (
+                <img
+                  className="vertical-timeline-element-image"
+                  alt={project.Name}
+                  src={project.picture}
+                />
+              )}
+              {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
+              <p className="vertical-timeline-element-desc">{project.Desc}</p>
             </VerticalTimelineElement>
           ))}
-        <VerticalTimelineElement
+        {/* <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
@@ -67,78 +79,7 @@ const Timeline = () => {
             Creative Direction, User Experience, Visual Design, Project
             Management, Team Leading
           </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={testIcon}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
+        </VerticalTimelineElement> */}
         <VerticalTimelineElement
           iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
           icon={testIcon}
